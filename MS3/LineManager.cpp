f7956a8feff4e3482e0d89b0d91d628f44569dfa
Harikrishna Paresh Patel <hpatel296@myseneca.ca>
2023-04-05 20:41:21 -0400

    Updated classes

--- MS3/LineManager.cpp
+++ MS3/LineManager.cpp
@@ -1,6 +1,103 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
 #include "LineManager.h"
+#include "Utilities.h"
 using namespace std;
 namespace sdds {
+   Utilities ut;
+   LineManager::LineManager(const std::string& file, const std::vector<Workstation*>& stations) {
+      ifstream File(file);
+      string data{};
+      size_t ps = 0;
+      bool other;
 
+      if (!File) {
+         throw string("Unable to open file:" + file);
+      }
+
+      while (!File.eof()) {
+         Workstation* curWorkStn{};
+         Workstation* nexWorkStn{};
+         string curStation, nexStation;
+
+         getline(File, data);
+
+         if (other) {
+            nexStation = ut.extractToken(data, ps, other);
+         }
+         
+         for_each(stations.begin(), stations.end(), [&curWorkStn, &nexWorkStn, curStation, nexStation](Workstation* Ws) {
+            if (Ws->getItemName() == nexStation) {
+               curWorkStn = Ws;
+            }
+            else if (Ws->getItemName() == nexStation) {
+               nexWorkStn = Ws;
+            }
+         });
+
+         if (m_activeLine.size() == 0) {
+            m_firstStation = curWorkStn;
+         }
+
+         curWorkStn->setNextStation(nexWorkStn);
+         m_activeLine.push_back(curWorkStn);
+      }
+      File.close();
+   }
+
+   void LineManager::reorderStations() {
+      vector<Workstation*> newStations;
+      Workstation* lastStation{};
+      size_t count = 0;
+
+      while (count < m_activeLine.size()) {
+         for (size_t i = 0; i < m_activeLine.size(); i++) {
+            newStations.push_back(m_activeLine[i]);
+            lastStation = m_activeLine[i];
+            count++;
+            break;
+         }
+      }
+
+      reverse(newStations.begin(), newStations.end());
+      m_firstStation = newStations[0];
+      m_activeLine = newStations;
+   }
+
+   bool LineManager::run(std::ostream& os) {
+      static int count = 0;
+      bool success = true;
+      count++;
+
+      os << "Line Manager Iteration: " << count << endl;
+
+      if (!g_pending.empty()) {
+         (*m_first_Station) += move(g_pending.front());
+         g_pending.pop_front();
+      }
+
+      for (unsigned int i = 0; i < m_activeLine.size(); i++) {
+         m_active_Line[i]->fill(os);
+      }
+
+      for (unsigned int i = 0; i < m_activeLine.size(); i++) {
+         m_activeLine[i]->attemptToMoveOrder();
+      }
+
+      for (unsigned int i = 0; i < m_activeLine.size(); i++) {
+         if (!m_activeLine[i]->success()) {
+            success = false;
+            break;
+         }
+      }
+
+      return success;
+   }
+
+   void LineManager::display(std::ostream& os) const {
+      for (unsigned i = 0; i < m_activeLine.size(); i++)
+         m_activeLine[i]->display(os);
+   }
 }
--- MS3/Workstation.cpp
+++ MS3/Workstation.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
 #include "Workstation.h"
+#include "Station.h"
 using namespace std;
 namespace sdds {
+   std::deque<CustomerOrder> g_pending;
+   std::deque<CustomerOrder> g_completed;
+   std::deque<CustomerOrder> g_incomplete;
 
+   Workstation::Workstation(const string& str) : Station{ str } {}
+
+   void Workstation::fill(std::ostream& os) {
+      if (!m_orders.empty())
+         m_orders.front().fillItem(*this, os);
+   }
+
+   bool Workstation::attemptToMoveOrder() {
+      bool success = false;
+      if (!m_orders.empty()) {
+         if (m_orders.front().isItemFilled(getItemName()) || this->getQuantity() == 0) {
+            if (m_pNextStation) {
+               *m_pNextStation += move(m_orders.front());
+            }
+            else {
+               if (m_orders.front().isOrderFilled()) {
+                  g_completed.emplace_back(move(m_orders.front()));
+               }
+               else {
+                  g_incomplete.emplace_back(move(m_orders.front()));
+               }
+            }
+            m_orders.pop_front();
+            success = true;
+         }
+      }
+      return success;
+   }
+
+   void Workstation::setNextStation(Workstation* station) {
+      m_pNextStation = station;
+   }
+
+   Workstation* Workstation::getNextStation() const {
+      return m_pNextStation;
+   }
+
+   void Workstation::display(std::ostream& os) const {
+      os << getItemName();
+      os << " --> ";
+      (m_pNextStation) ? 
+         os << m_pNextStation->getItemName() : os << "End of Line";
+      os << endl;
+   }
+
+   Workstation& Workstation::operator+=(CustomerOrder&& newOrder) {
+      m_orders.emplace_back(move(newOrder));
+      return *this;
+   }
 }
